7c417ad81c96c92c968119497e31e1300308d4fa
ivankp <ivan.pogrebnyak@gmail.com>
2021-07-25 14:46:59 -0400

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+const _id = id => document.getElementById(id);
+function make(p,...tags) {
+  for (const t of tags)
+    p = p.appendChild(document.createElement(t))
+  return p;
+}
+function clear(x) {
+  for (let c; c = x.firstChild; ) x.removeChild(c);
+  return x;
+}
+const round = x => x.toFixed(4).replace(/\.?0*$/,'');
+const last = xs => xs[xs.length-1];
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (const id of ['contact','bib']) {
+    _id(id).onclick = function(){
+      fetch('pages/'+id+'.html', { method: 'GET' })
+      // .then(r => r.json())
+      .then(r => { this.innerHTML = r; });
+    };
+  }
+});
