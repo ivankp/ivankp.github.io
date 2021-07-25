647e5794f8d86aec057f8ff06513ce6292ad0e62
ivankp <ivan.pogrebnyak@gmail.com>
2021-07-25 14:58:14 -0400

    3

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ const round = x => x.toFixed(4).replace(/\.?0*$/,'');
 const last = xs => xs[xs.length-1];
 
 document.addEventListener('DOMContentLoaded', () => {
+  const main = _id('main');
   for (const id of ['contact','bib']) {
     _id(id).onclick = function(){
       fetch('pages/'+id+'.html', { method: 'GET' })
-      // .then(r => r.json())
-      .then(r => { this.innerHTML = r; });
+      .then(r => r.text())
+      .then(r => { main.innerHTML = r; });
     };
   }
 });
